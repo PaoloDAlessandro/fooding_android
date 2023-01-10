a54b1ba8aa89215f1f510fc2f5d7b01a7dd2ba9d
Paolo Dalex <paolodalex96@gmail.com>
2023-01-11 00:52:35 +0100

    feat: start RicettaDetails ui layout

---
+++ app/src/main/java/it/itsar/fooding/RicettaDetails.java
@@ -0,0 +1,14 @@
+package it.itsar.fooding;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RicettaDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ricetta_details);
+    }
+}
