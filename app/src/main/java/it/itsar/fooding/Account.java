f19c9690d0d0e3304a5b67a3dd590860952040e9
Paolo Dalex <paolodalex96@gmail.com>
2023-02-12 17:37:14 +0100

    feat: init account ui

--- app/src/main/java/it/itsar/fooding/Account.java
+++ app/src/main/java/it/itsar/fooding/Account.java
@@ -7,21 +7,26 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
 
 import org.checkerframework.checker.units.qual.A;
+import org.w3c.dom.Text;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Account extends Fragment {
 
@@ -34,6 +39,8 @@ public class Account extends Fragment {
     private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
     private ImageButton logoutButton;
+    private TextView userGreating;
+    private ImageView userImage;
 
     private BottomNavigationView bottomNavigationView;
 
@@ -52,6 +59,25 @@ public class Account extends Fragment {
         fragmentManager = getParentFragmentManager();
         logoutButton = getActivity().findViewById(R.id.logoutIcon);
         bottomNavigationView = getActivity().findViewById(R.id.bottomMenu);
+        userGreating = view.findViewById(R.id.userName);
+        userImage = view.findViewById(R.id.userImage);
+
+        if (firebaseAuth.getCurrentUser().getPhotoUrl() != null) {
+            downloadImage(Objects.requireNonNull(Objects.requireNonNull(firebaseAuth.getCurrentUser()).getPhotoUrl()).toString());
+        }
+
+
+        String username = Objects.requireNonNull(firebaseAuth.getCurrentUser()).getDisplayName();
+
+        if (username != null) {
+            int spaceIndex = username.indexOf(" ");
+            if (spaceIndex != -1) {
+                userGreating.setText("Ciao " + username.substring(0, spaceIndex) + "!");
+            } else if (myProperties.getUserUsername() != null) {
+                username = myProperties.getUserUsername();
+                userGreating.setText("Bentornato " + username + "!");
+            }
+        }
 
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,7 +92,6 @@ public class Account extends Fragment {
                 configFragmentManager(Login.class);
             }
         });
-
     }
 
     private void configFragmentManager(Class fragmentClass) {
@@ -76,6 +101,20 @@ public class Account extends Fragment {
                 .setReorderingAllowed(true)
                 .addToBackStack("name")
                 .commit();
+    }
+
+    private void downloadImage(String imageUri) {
+        Picasso.get()
+                .load(imageUri)
+                .into(userImage, new com.squareup.picasso.Callback() {
+                    @Override
+                    public void onSuccess(){
+                    }
+
+                    @Override
+                    public void onError(Exception e) {
+                    }
+                });
     }
 
 }
